6516a9798d7a528f07b81ef2b9f6368dbd948ec0
ssmn <143730679+ssmn93@users.noreply.github.com>
2025-09-15 19:19:14 +0900

    Update 059-ticket.js

--- commands/059-ticket.js
+++ commands/059-ticket.js
@@ -91,6 +91,7 @@ module.exports = {
 // --- ボタン押下時の処理 ---
 client.on('interactionCreate', async interaction => {
     if (interaction.isButton()) {
+      if (interaction.component.label !== "生成" && interaction.component.label !== "クローズする") return;
         const tickets = JSON.parse(fs.readFileSync(ticketFilePath, 'utf8'));
         const ticketEntry = tickets.find(entry => entry.messageId === interaction.message.id);
         const closes = JSON.parse(fs.readFileSync(closeFilePath, 'utf8'));
